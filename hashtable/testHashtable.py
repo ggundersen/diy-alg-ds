36405725df9556ce0a1c8384878466b52a408ed5
gwg <ggundersen@gmail.com>
2014-08-23 14:41:49 -0400

    Unit tests for get and put

--- hashtable/testHashtable.py
+++ hashtable/testHashtable.py
@@ -2,16 +2,25 @@
 
 import unittest
 
-import hashtable
+from hashtable import Hashtable
 
 
 class TestHashtable(unittest.TestCase):
 
-    def setUp(self):
-        pass
 
-    def test_test(self):
-        pass
+    def setUp(self):
+        self.ht = Hashtable()
+
+
+    def test_put(self):
+        self.assertEqual(self.ht._size(), 0)
+        self.ht.put('foo', 100)
+        self.assertEqual(self.ht._size(), 1)
+
+    def test_get(self):
+        self.ht.put('foo', 100)
+        val = self.ht.get('foo')
+        self.assertEqual(val, 100)
 
 
 if __name__ == '__main__':
