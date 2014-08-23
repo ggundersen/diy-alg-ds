8236bbf970c31b869cb9c977ad5e43e56718cdd6
gwg <ggundersen@gmail.com>
2014-08-23 15:46:10 -0400

    Initialized tree and binarytree with API

---
+++ tree/binarytree.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""----------------------------------------------------------------------------
+binarytree.py
+
+A binary tree is a tree, the abstract data type, with a invariant that each
+node has at most two child nodes. The root node is the only node in a tree
+without a parent.
+----------------------------------------------------------------------------"""
+
+
+class Binarytree:
+
+
+    class Node:
+
+        def __init__(self, left=None, right=None):
+            self.val = val
+            self.left = left
+            self.right = right
+
+
+    def __init__(self):
+        pass
+
+
+    def size(self):
+        pass
+
+
+    def get(key):
+        pass
+
+
+    def put(key, val):
+        pass
