da318ebc94af86c7a3411bca43067fa71dec42a2
gwg <ggundersen@gmail.com>
2014-08-23 08:48:16 -0400

    Stable hashtable without resizing

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -10,8 +10,6 @@ distinct keys, the event is called a "collision."
 
 Hashing is a good example of a space-time tradeoff. With infinite space, we
 could avoid all collisions. With infinite time, we could search sequentially.
-
-This hash table relies on linear probing for collision resolution.
 ----------------------------------------------------------------------------"""
 
 import pdb
@@ -22,36 +20,51 @@ class Hashtable:
 
     def __init__(self):
         """Initialize empty hashtable."""
-        self.N = 0   # Number of key-value pairs in the table
-        self.M = 2  # Size of linear probing table; M > N
-        self.a = [None] * self.M
+        self.N = 0  # Number of key-value pairs in the hash table
+        self.a = [[]] * 1000
 
 
     def _hash(self, val):
         """Return hash code after ensuring it is positive and within range."""
-        return abs(hash(val)) % self.M
+        return abs(hash(val)) % len(self.a)
 
 
-    def _resize(self, lim):
-        """Doubles size of internal array."""
-        temp = [None] * lim
-        for i, v in enumerate(self.a):
-            temp[i] = self.a[i]
-        self.a = temp
+    #def _resize(self, lim):
+    #    """Resizes internal array."""
+    #    temp = [[]] * lim
+    #    for i, v in enumerate(self.a):
+    #        temp[i] = self.a[i]
+    #    self.a = temp
 
 
     def put(self, key, val):
         """Add val at index hashed from key. Return void."""
-        if self.N >= self.M / 2:
-            self._resize(2 * self.M)
-        # TODO: Deal with collisions
-        self.a[self._hash(key)] = val
-        self.N += 1
+        #if len(self.a) == self.N:
+        #    self._resize(2 * self.N)
+
+        idx = self._hash(key)
+        chain = self.a[idx]
+        if len(chain) == 0:
+            chain.append((key, val))
+            self.N += 1
+        else:
+            not_added = True
+            for tple in chain:
+                if tple[0] == key:
+                    not_added = False
+                    break
+            if not_added:
+                chain.append((key, val))
+                self.N += 1
 
 
     def get(self, key):
         """Return value stored at the index of hashed key."""
-        return self.a[self._hash(key)]
+        chain = self.a[self._hash(key)]
+        for tple in chain:
+            if tple[0] == key:
+                return tple[1]
+        return None
 
 
 if __name__ == '__main__':
