dbadf7762fa9f4abfe2b0af275ad131ca361d272
gwg <ggundersen@gmail.com>
2014-08-23 15:34:43 -0400

    Initializing bag

---
+++ bag/bag.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""----------------------------------------------------------------------------
+bag.py
+
+A bag is a collection where order and removing items are not supported. A bag's
+primary function is to be iterated over.
+----------------------------------------------------------------------------"""
+
+
+class Bag:
+
+
+    def __init__(self):
+        pass
+
+    
+    def isEmpty(self):
+        pass
+
+
+    def size(self):
+        pass
+
+    
+    def add(self):
+        pass
+
+
+    def each(self):
+        pass
