ba1329707bf3d425cfa90c83eeaca99f6acb08fc
gwg <ggundersen@gmail.com>
2014-08-23 15:45:44 -0400

    Added docstrings

--- bag/bag.py
+++ bag/bag.py
@@ -12,23 +12,29 @@ class Bag:
 
 
     def __init__(self):
+        """Construct empty bag."""
         self.N = 0
         self.a = []
 
     
     def isEmpty(self):
+        """Return true if the bag is empty, false otherwise."""
         return self.N == 0
 
 
     def size(self):
+        """Return size of bag."""
         return self.N
 
     
     def add(self, item):
+        """Add item of any type to bag."""
         self.N += 1
         self.a.append(item)
 
 
+    # Is this functional style Pythonic?
     def each(self, fn):
+        """Call fn on each item in the bag."""
         for item in self.a:
             fn(item)
