32cd7cc0fe4f26ad71233760423b7cb0c9b094bf
gwg <ggundersen@gmail.com>
2014-08-22 22:08:22 -0400

    Stubbed an API

--- hashtable.py
+++ hashtable.py
@@ -3,4 +3,36 @@
 """----------------------------------------------------------------------------
 hashtable.py
 
+A hash table is a data structure that implements an associative array, mapping
+keys to values. A Hash function efficiently generates indexes from the keys
+added to the hash table. When a hash function produces the same index from two
+distinct keys, the event is called a "collision."
+
+Hashing is a good example of a space-time tradeoff. With infinite space, we
+could avoid all collisions. With infinite time, we could search sequentially.
 ----------------------------------------------------------------------------"""
+
+import hashlib
+
+
+class Hashtable:
+
+
+    def __init__(self):
+        pass
+
+
+    def _hash(self):
+        pass
+
+
+    def put(self):
+        pass
+
+
+    def get(self):
+        pass
+
+
+if __name__ == 'main':
+    ht = Hashtable()
