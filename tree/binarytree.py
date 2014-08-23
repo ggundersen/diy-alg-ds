e0f2c17a9055a49b16882e313e44a38bb7ea9759
gwg <ggundersen@gmail.com>
2014-08-23 15:58:21 -0400

    Docstrings and private function stubs.

--- tree/binarytree.py
+++ tree/binarytree.py
@@ -13,24 +13,40 @@ class Binarytree:
 
 
     class Node:
-
+        """Inner class to represent nodes in tree."""
+        
+        
         def __init__(self, left=None, right=None):
+            self.root = None
             self.val = val
             self.left = left
             self.right = right
+            self.N
 
 
     def __init__(self):
+        """Initialize empty binary tree."""
         pass
 
 
     def size(self):
+        """Return size based on node's N property."""
+        if self.root == None:
+            return 0
+        return self.root.N
+
+
+    def get(self, key):
+        """Get value based on key."""
         pass
 
 
-    def get(key):
+    def _put(self, key, val):
+        """Add key-value pair. Recursively return each subtree's root."""
         pass
 
 
-    def put(key, val):
-        pass
+    def put(self, key, val):
+        """Recursively delegate to private _put(). Return void."""
+        self.root = _put(key, val)
+        
