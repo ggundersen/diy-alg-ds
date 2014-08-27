978a1ef161b8d1a897cdcb590c6b35f46d81273e
gwg <ggundersen@gmail.com>
2014-08-27 08:38:40 -0400

    Add_first() and add_at() complete.

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -9,15 +9,18 @@ class Linkedlist:
    
 
     class Node:
-        def __init__(self, item, next=None):
+
+        def __init__(self, item, next=None, prev=None):
             """Initialize node with default 'pointer' to None."""
-            self.item = None
+            self.item = item
             self.next = next
+            self.prev = prev
 
     
     def __init__(self):
         """Initialize empty linked list."""
         self.first = None
+        self.last = None
         self.N = 0
 
 
@@ -32,15 +35,58 @@ class Linkedlist:
 
 
     def add_first(self, item):
-        """Add item as first."""
+        """Add item to end of list."""
         if self.is_empty():
-            self.first = Node(item)
+            self.first = self.Node(item, self.last)
         else:
             temp = self.first
-            self.first = Node(item, temp)
+            self.first = self.Node(item, temp)
+            temp.prev = self.first
         self.N += 1
 
 
-    def remove_all(self, item):
+    def add_at(self, item, idx):
+        """Add item at index."""
+        if self.is_empty():
+            self.first = self.Node(item, self.last)
+            return
+        elif self.size() < idx:
+            self.last = self.Node(item, None, self.last) 
+            return
+
+        count = 0
+        node = self.first
+        while node is not None and node.next is not None and count < idx:
+            node = node.next
+            count += 1
+        new_node = self.Node(item, node, node.prev)
+        node.prev.next = new_node
+        
+
+
+    def get_first(self, item):
         """Remove all nodes with item."""
-        pass
+        result = self.first
+        if self.first.next is not None:
+            self.first = self.first.next
+        return result
+
+
+    def pp(self):
+        """Pretty print linked list."""
+        count = 0
+        node = self.first
+        while node is not None:
+            print(str(count) + ': ' + str(node.item))
+            node = node.next
+            count += 1
+
+
+if __name__ == '__main__':
+    ll = Linkedlist()
+    ll.add_first('foo')
+    ll.add_first('bar')
+    ll.add_first('qux')
+    ll.add_at('baz', 1)
+    ll.pp()
+
