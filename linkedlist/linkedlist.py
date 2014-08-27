b6b57ad0b5aa7934c6f531af5b9d99f0ece059c2
gwg <ggundersen@gmail.com>
2014-08-27 07:59:44 -0400

    Added docstrings. Rely on is_empty() for guard in add_first() and remove_all().

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -10,34 +10,37 @@ class Linkedlist:
 
     class Node:
         def __init__(self, item, next=None):
+            """Initialize node with default 'pointer' to None."""
             self.item = None
             self.next = next
 
     
     def __init__(self):
+        """Initialize empty linked list."""
         self.first = None
         self.N = 0
 
 
     def is_empty(self):
-        return self.N != 0
+        """Return true if list is empty, false otherwise."""
+        return self.first == None
 
 
     def size(self):
+        """Return number of nodes."""
         return self.N
 
 
-    def add(self, item):
-        if self.first == None:
+    def add_first(self, item):
+        """Add item as first."""
+        if self.is_empty():
             self.first = Node(item)
         else:
-            self._add(self.first, item)
-        
-        #else:
-        #    temp = self.first
-        #    self.first = Node(item, temp)
-        #self.N += 1
+            temp = self.first
+            self.first = Node(item, temp)
+        self.N += 1
 
 
-    def remove(self):
+    def remove_all(self, item):
+        """Remove all nodes with item."""
         pass
