6d6773edd6fe0b9a408ba4268ed15d42fcd82d2f
gwg <ggundersen@gmail.com>
2014-08-27 21:11:30 -0400

    Unit tests for binarytree.py.

--- tree/testBinarytree.py
+++ tree/testBinarytree.py
@@ -20,9 +20,18 @@ class TestBinarytree(unittest.TestCase):
         self.bt.put('qux', 7)
         self.assertEqual(self.bt.size(), 3)
 
+    def test_put_multiple_types_raises_error(self):
+        self.bt.put('foo', 3)
+        self.assertRaises(TypeError, self.bt.put, (5, 'baz'))
+
     def test_get(self):
         self.bt.put('foo', 3)
         self.assertEqual(self.bt.get('foo'), 3)
+
+    def test_size(self):
+        self.bt.put('foo', 3)
+        self.assertEqual(self.bt.size(), 1)
+
 
 
 if __name__ == '__main__':
