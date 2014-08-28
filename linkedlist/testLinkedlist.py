db7474806d00d046a768cda7e0d3e174aa9f6071
gwg <ggundersen@gmail.com>
2014-08-27 22:41:11 -0400

    Unit tests for remove methods.

--- linkedlist/testLinkedlist.py
+++ linkedlist/testLinkedlist.py
@@ -43,12 +43,34 @@ class TestLinkedlist(unittest.TestCase):
         self.ll.add_first(99)
         self.assertEqual(self.ll.get_at(1), 4)
 
+    def test_get_at_empty(self):
+        self.assertEqual(self.ll.get_at(99), None)
+
+    def test_get_at_big_index(self):
+        self.ll.add_first(3)
+        self.assertEqual(self.ll.get_at(99), None)
+
     def test_remove_first(self):
         self.ll.add_first(3)
         self.ll.add_first(4)
         item = self.ll.remove_first()
         self.assertEqual(item, 4)
         self.assertEqual(self.ll._expose(), '3')
+
+    def test_remove_at(self):
+        self.ll.add_first(3)
+        self.ll.add_first(4)
+        self.ll.add_first(99)
+        val = self.ll.remove_at(1)
+        self.assertEqual(val, 4)
+        self.assertEqual(self.ll._expose(), '99-3')
+
+    def test_remove_at_empty(self):
+        self.assertEqual(self.ll.remove_at(99), None)
+
+    def test_remove_at_big_index(self):
+        self.ll.add_first(3)
+        self.assertEqual(self.ll.remove_at(99), None)
         
 
 if __name__ == '__main__':
