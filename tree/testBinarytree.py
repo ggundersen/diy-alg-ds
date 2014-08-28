bad4cc89b4efdc9898f264dc820f5ad6cef19f1c
gwg <ggundersen@gmail.com>
2014-08-27 20:41:15 -0400

    Initializing unit tests.

---
+++ tree/testBinarytree.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import unittest
+
+from binarytree import Binarytree
+
+
+class TestBinarytree(unittest.TestCase):
+
+
+    def setUp(self):
+        self.bt = Binarytree()
+
+
+if __name__ == '__main__':
+    unittest.main()
