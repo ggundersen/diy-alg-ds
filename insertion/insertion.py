bbc18de07f00b22e173022da678e286e8df053d4
gwg <ggundersen@gmail.com>
2014-08-28 18:28:41 -0400

    Debugged insertion sort. Works for basic cases.

--- insertion/insertion.py
+++ insertion/insertion.py
@@ -14,21 +14,14 @@ class Insertion:
 
     @staticmethod
     def sort(lst):
-        print(lst)
+        """Insertion sort list. Return list."""
         N = len(lst)
-        for i in range(N):
-            curr = lst[i]
-            print('curr: ' + str(curr))
-            for j in range(i, -1, -1):
-                if lst[j] > lst[i]:
-                    print('breaking early: --------')
-                    print(lst[i])
-                    print(lst[j])
-                    print('--------')
+        for i in range(1, N):
+            for j in range(i, 0, -1):
+                if lst[j-1] < lst[j]:
                     break
-                Insertion._exch(lst, j, i)
-            print(lst)
-        print(lst)
+                Insertion._exch(lst, j-1, j)
+        return lst
 
     @staticmethod
     def _exch(lst, a, b):
@@ -39,4 +32,4 @@ class Insertion:
 
 
 if __name__ == '__main__':
-    Insertion.sort([4,5,2,3])
+    print(Insertion.sort([4,5,99,3]))
