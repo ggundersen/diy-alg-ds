1cb7d86bac8243e53112af30c7c14e8571fcb2da
gwg <ggundersen@gmail.com>
2014-08-27 20:53:47 -0400

    Removed extra spacing based on PEP8.

--- bag/bag.py
+++ bag/bag.py
@@ -10,28 +10,23 @@ primary function is to be iterated over.
 
 class Bag:
 
-
     def __init__(self):
         """Construct empty bag."""
         self._N = 0
         self._a = []
-
     
     def is_empty(self):
         """Return true if the bag is empty, false otherwise."""
         return self._N == 0
 
-
     def size(self):
         """Return size of bag."""
         return self._N
-
     
     def add(self, item):
         """Add item of any type to bag."""
         self._N += 1
         self._a.append(item)
-
 
     # Is this functional style Pythonic?
     def each(self, fn):
--- bag/testBag.py
+++ bag/testBag.py
@@ -7,27 +7,22 @@ from bag import Bag
 
 class TestBag(unittest.TestCase):
 
-
     def setUp(self):
         self.bag = Bag()
-
 
     def test_is_empty(self):
         self.assertTrue(self.bag.is_empty())
         self.bag.add(3)
         self.assertTrue(not self.bag.is_empty())
 
-    
     def test_size(self):
         self.assertEqual(self.bag.size(), 0)
         self.bag.add(3)
         self.assertEqual(self.bag.size(), 1)
 
-
     def test_add(self):
         self.bag.add(3)
         self.assertEqual(self.bag._a, [3])  # This code smells.
-
 
     def test_each(self):
         self.bag.add(3)
