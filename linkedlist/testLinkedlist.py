26f4d3eb2f2f3bbf150c37a88c700d1e195e6f4e
gwg <ggundersen@gmail.com>
2014-08-27 21:25:56 -0400

    Initializing unit tests.

---
+++ linkedlist/testLinkedlist.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import unittest
+
+from linkedlist import Linkedlist
+
+
+class TestLinkedlist(unittest.TestCase):
+
+    def setUp(self):
+        self.ll = Linkedlist()
+
+
+if __name__ == '__main__':
+    unittest.main()
