07c2fa47cfc426aecec96a936ba7f20ce4dec847
gwg <ggundersen@gmail.com>
2014-08-27 20:08:23 -0400

    Initializing unit tests.

---
+++ bag/testBag.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import unittest
+
+from bag import Bag
+
+
+class TestBag(unittest.TestCase):
+
+
+    def setUp(self):
+        self.bag = Bag()
+
+
+    def test_put(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
