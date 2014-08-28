095f1bbe0e8d7ddacba2d03b887790457e012cbf
gwg <ggundersen@gmail.com>
2014-08-27 20:12:17 -0400

    Added tests for is_empty() and size().

--- bag/testBag.py
+++ bag/testBag.py
@@ -12,8 +12,16 @@ class TestBag(unittest.TestCase):
         self.bag = Bag()
 
 
-    def test_put(self):
-        pass
+    def test_is_empty(self):
+        self.assertTrue(self.bag.is_empty())
+        self.bag.add(3)
+        self.assertTrue(not self.bag.is_empty())
+
+    
+    def test_size(self):
+        self.assertEqual(self.bag.size(), 0)
+        self.bag.add(3)
+        self.assertEqual(self.bag.size(), 1)
 
 
 if __name__ == '__main__':
